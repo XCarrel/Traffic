5ac13938427c6ea751f047fb8b6f05b6c3f3c683
MisterX25 <Xavier.Carrel@cpnv.ch>
2020-08-17 15:31:32 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * File : index.php
+ * Author : X. Carrel
+ * Created : 2020-08-17
+ * Modified last :
+ **/
+
+$red = true;
+$yellow = false;
+$green = false;
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Traffic</title>
+</head>
+<body>
+<div class="container"
+</body>
+</html>
+
